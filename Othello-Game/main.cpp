a3d6c42b92b886fa382b489ba455d95f0141eddb
Wucashade <98740752+Wucashade@users.noreply.github.com>
2023-01-13 23:32:50 +0000

    Forgot to return to the old state so did that
    Also finally fixed the error that caused the window to crash several times by initialising the Window object

--- Othello-Game/main.cpp
+++ Othello-Game/main.cpp
@@ -1,30 +1,33 @@
-﻿// Othello-Game.cpp : Defines the entry point for the application.
+﻿// main.cpp : Defines the entry point for the application.
 //
 
-#include "SDL.h" 
+#include <iostream>
+#include <SDL.h>
+#include "Window.hpp"
+#include <string>
+
+
+using namespace std;
+const int WIDTH = 800, HEIGHT = 600;
 
 int main(int argc, char* argv[])
 {
-    SDL_Init(SDL_INIT_VIDEO);
 
-    SDL_Window* window = SDL_CreateWindow(
-        "SDL2Test",
-        SDL_WINDOWPOS_UNDEFINED,
-        SDL_WINDOWPOS_UNDEFINED,
-        640,
-        480,
-        0
-    );
+    Window* window = new Window;
+    window->init("Othello", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
 
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-    SDL_RenderClear(renderer);
-    SDL_RenderPresent(renderer);
 
-    SDL_Delay(3000);
+    while (window->running())
+    {
+        window->eventManager();
 
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    }
+
+    window->clean();
+    delete window;
 
     return 0;
-}
+
+
+
+};
