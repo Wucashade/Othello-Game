f5ac824bdb5705a20ee958d5832145d101cf3eda
Wucashade <98740752+Wucashade@users.noreply.github.com>
2023-01-13 21:12:39 +0000

    Add project files.

---
+++ Othello-Game/Othello-Game.cpp
@@ -0,0 +1,12 @@
+﻿// Othello-Game.cpp : Defines the entry point for the application.
+//
+
+#include "Othello-Game.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
