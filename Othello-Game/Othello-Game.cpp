7cf442f6563dcf6fa169995930d8e08cec636064
Wucashade <98740752+Wucashade@users.noreply.github.com>
2023-01-13 23:07:07 +0000

    Fixed all settings and make sure that SDL works correctly so that I can work on the rest

--- Othello-Game/Othello-Game.cpp
+++ Othello-Game/Othello-Game.cpp
@@ -1,12 +1,30 @@
 ﻿// Othello-Game.cpp : Defines the entry point for the application.
 //
 
-#include "Othello-Game.h"
+#include "SDL.h" 
 
-using namespace std;
-
-int main()
+int main(int argc, char* argv[])
 {
-	cout << "Hello CMake." << endl;
-	return 0;
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window* window = SDL_CreateWindow(
+        "SDL2Test",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        640,
+        480,
+        0
+    );
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+
+    SDL_Delay(3000);
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
 }
