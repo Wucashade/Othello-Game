78a91cd0a1d515660dddaab86392d2c9a6ef2320
Wucashade <lukewasela@gmail.com>
2023-03-17 19:11:37 +0000

    Shift function changes and move generation
    Shift function changes - Centralised the shift functions so that now instead of having one for each direction, there is just one which takes the list of directions stored in an array and the chosen bitboard and does the shift based on the chosen direction, along with applying a mask.
    
    Move generation - another step for move generation has been completed,  using the Dump7Fill algorithm it looks at each direction of a piece: horizontally, vertically and diagonally and looks at where the next free space is between it and all the opponent pieces in the way. It then adds to a legalMoves variable and then returns that variable once all moves have been found.
    
    printSingleBitboard function - added a function to better see single bitboards with either one or zero bits printed  so that i can use it for other bitboards than just black or white pieces.
    
    Small changes - changed the macro names to SCREAMING_SNAKE_CASE standard, added some comments, changed the squares enum format to correctly print the bitboard

--- Othello-Game/bitboards.cpp
+++ Othello-Game/bitboards.cpp
@@ -7,8 +7,8 @@
 #include <string>
 
 typedef uint64_t U64;
-#define getBit(bb, square) (bb & (1ULL << square))
-#define setBit(bb, square) (bb |= (1ULL << square))
+#define GET_BIT(bb, square) (bb & (1ULL << square))
+#define SET_BIT(bb, square) (bb |= (1ULL << square))
 
 using namespace std;
 
@@ -17,7 +17,7 @@ U64 bitboardWhite = 0ULL;
 U64 bitboardBlack = 0ULL;
 U64 emptyBB;
 U64 occupiedBB;
-
+/*
 //Board representation as an enum
 enum squares {
   a1, b1, c1, d1, e1, f1, g1, h1,
@@ -29,6 +29,17 @@ enum squares {
   a7, b7, c7, d7, e7, f7, g7, h7,
   a8, b8, c8, d8, e8, f8, g8, h8
 };
+*/
+enum squares {
+  h8, g8, f8, e8, d8, c8, b8, a8,
+  h7, g7, f7, e7, d7, c7, b7, a7,
+  h6, g6, f6, e6, d6, c6, b6, a6,
+  h5, g5, f5, e5, d5, c5, b5, a5,
+  h4, g4, f4, e4, d4, c4, b4, a4,
+  h3, g3, f3, e3, d3, c3, b3, a3,
+  h2, g2, f2, e2, d2, c2, b2, a2,
+  h1, g1, f1, e1, d1, c1, b1, a1
+};
 
 enum colours{DISKWHITE, DISKBLACK, DISKEMPTY};
 
@@ -36,49 +47,38 @@ enum colours{DISKWHITE, DISKBLACK, DISKEMPTY};
     Masks to ignore moves that go out of bitboard bounds
 */
 
-const U64 notLeft = 0xFEFEFEFEFEFEFEFEULL,
-          notRight = 0x7F7F7F7F7F7F7F7FULL,
-          notDownRight = 0x007F7F7F7F7F7F7FULL,
-          notDown = 0xFFFFFFFFFFFFFFFFULL,
-          notDownLeft = 0x00FEFEFEFEFEFEFEULL,
-          notUpLeft = 0xFEFEFEFEFEFEFE00ULL,
-          notUp = 0xFFFFFFFFFFFFFFFFULL,
-          notUpRight = 0x7F7F7F7F7F7F7F00ULL;
+U64 avoidWrap[8] =
+{
+   0xfefefefefefefe00, //UP-LEFT
+   0xfefefefefefefefe, //LEFT
+   0x00fefefefefefefe, //DOWN-LEFT
+   0x00ffffffffffffff, //DOWN
+   0x007f7f7f7f7f7f7f, //DOWN-RIGHT
+   0x7f7f7f7f7f7f7f7f, //RIGHT
+   0x7f7f7f7f7f7f7f00, //UP-RIGHT
+   0xffffffffffffff00, //UP
+};
+
+int shift[8] = {9, 1,-7,-8,-9,-1, 7, 8}; 
 
 /*
-    Different bitboard shifts to simulate movement
+   9     8    7
+     \   |   /
+      \  | /
+  1 --- Bit --- -1
+       / | \
+     /   |   \
+   -7    -8   -9
 */
-U64 northShift (U64 bb) 
-{
-    return  bb << 8;
-}
-U64 northEastShift (U64 bb) 
-{
-    return  (bb & notUpRight) << 7;
-}
-U64 eastShift (U64 bb) 
-{
-    return  (bb & notRight) >> 1;
-}
-U64 southEastShift (U64 bb) 
-{
-    return  (bb & notDownRight) >> 9;
-}
-U64 southShift (U64 bb) 
-{
-    return  bb >> 8;
-}
-U64 southWestShift (U64 bb) 
-{
-    return  (bb & notDownLeft) >> 7;
-}
-U64 westShift (U64 bb) 
-{
-    return  (bb & notLeft) << 1;
-}
-U64 northWestShift (U64 bb) 
-{
-    return  (bb & notUpLeft) << 9;
+
+U64 shiftOne (U64 bb, int dir8) {
+    if(dir8 < 0 || dir8 > 7)
+    {
+        cout << "Invalid direction" << endl;
+    }
+    int r = shift[dir8]; // {+-1,7,8,9}
+    return (r > 0) ? ((bb << r)&avoidWrap[dir8]):((bb >> -r)&avoidWrap[dir8]);
+
 }
 
 //Function to print out the current bitboard
@@ -93,9 +93,9 @@ void printBitboard(U64 bbOne, U64 bbTwo)
         for(int j = 7; j > -1; j--)
         {
             int square = i * 8 + j;
-            cout << (getBit(bbOne, square) ? "W":(getBit(bbTwo, square) ? "B":"0")) << " ";
+            cout << (GET_BIT(bbOne, square) ? "W":(GET_BIT(bbTwo, square) ? "B":"0")) << " ";
             /*
-            //cout << ( getBit(bb, square) ? 1:0 )<< " ";
+            //cout << ( GET_BIT(bb, square) ? 1:0 )<< " ";
             
             if((bbOne & (1ULL << square)) == 1)
             {
@@ -103,7 +103,7 @@ void printBitboard(U64 bbOne, U64 bbTwo)
                 
             }
             
-            else if (getBit(bbTwo, square) == 1)
+            else if (GET_BIT(bbTwo, square) == 1)
             {
                 cout << "B ";
             }
@@ -113,6 +113,22 @@ void printBitboard(U64 bbOne, U64 bbTwo)
             }
 
             */
+        }
+        cout << "" << endl;
+    }
+    cout << endl << "   a b c d e f g h" << endl;
+}
+
+void printSingleBitboard(U64 bb)
+{
+    for (int i = 7; i > -1; i--)
+    {
+        cout << 8-i << "  ";
+        for(int j = 7; j > -1; j--)
+        {
+            int square = i * 8 + j;
+            cout << ( GET_BIT(bb, square) ? 1:0 )<< " ";
+
         }
         cout << "" << endl;
     }
@@ -139,8 +155,25 @@ int popCount(U64 bb)
 
 U64 generateMoves(U64 bbOwn, U64 bbOpponent)
 {
+    emptyBB = ~(bbOwn | bbOpponent);
+    U64 moves;
+    U64 legalMoves = 0;
 
+    for(int i = 0; i < 8; i++)
+    {
 
+        moves = shiftOne(bbOwn, i) & bbOpponent;
+
+        moves |= shiftOne(moves, i) & bbOpponent;
+        moves |= shiftOne(moves, i) & bbOpponent;
+        moves |= shiftOne(moves, i) & bbOpponent;
+        moves |= shiftOne(moves, i) & bbOpponent;
+        moves |= shiftOne(moves, i) & bbOpponent;
+
+        legalMoves |= shiftOne(moves, i) & emptyBB;
+
+    }
+    return legalMoves;
 
 }
 
@@ -148,10 +181,11 @@ int main()
 {
 
     int cnt = 0;
-    setBit(bitboardBlack, e5);
-    setBit(bitboardBlack, d4);
-    setBit(bitboardWhite, e4);
-    setBit(bitboardWhite, d5);
+    SET_BIT(bitboardWhite, d4);
+    SET_BIT(bitboardWhite, e5);
+    SET_BIT(bitboardBlack, e4);
+    SET_BIT(bitboardBlack, d5);
+    SET_BIT(bitboardWhite, c4);
     
     printBitboard(bitboardWhite, bitboardBlack);
 
@@ -160,19 +194,10 @@ int main()
     cnt = popCount(bitboardWhite);
     cout << "WHITE: "<<cnt << endl;
 
-    setBit(bitboardBlack, a5);
-    setBit(bitboardBlack, h8);
 
-    printBitboard(bitboardWhite, bitboardBlack);
+    printSingleBitboard(generateMoves(bitboardBlack, bitboardWhite));
 
-    cnt = popCount(bitboardBlack);
-    cout << "BLACK: "<<cnt << endl;
-    cnt = popCount(bitboardWhite);
-    cout << "WHITE: "<<cnt << endl;
-
-    bitboardWhite=northShift(bitboardWhite);
-    printBitboard(bitboardWhite, bitboardBlack);
-
+    
     return 0;
 
 
